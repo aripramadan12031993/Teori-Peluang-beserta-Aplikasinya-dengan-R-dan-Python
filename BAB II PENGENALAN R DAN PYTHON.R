526d540392e88757a6cb2866166029c259c77f49
aripramadan12031993 <aripramadan@gmail.com>
2025-09-03 11:13:08 +0700

    Update BAB II PENGENALAN R DAN PYTHON.R

---
+++ BAB II PENGENALAN R DAN PYTHON.R
@@ -0,0 +1,13 @@
+#2.3.1	“Hello World” dan Komentar (Comment)
+#Kode Praktik – R 
+print("Hello World")
+# Ini adalah komentar
+
+#Output– R
+#Hello World
+
+#2.3.2	Variabel dan Tipe Data
+Kode Praktik – R 
+x <- 10        # Numeric
+y <- "R"       # Character
+z <- TRUE      # Logical
